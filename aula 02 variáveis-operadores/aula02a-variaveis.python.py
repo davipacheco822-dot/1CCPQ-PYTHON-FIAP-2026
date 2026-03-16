005433a18dc569af3894abac6c9fb6e339698157
davipacheco822-dot <davipacheco822@gmail.com>
2026-03-16 20:45:51 -0300

    aula 2 - variáveis

---
+++ aula 02 variáveis-operadores/aula02a-variaveis.python.py
@@ -0,0 +1,22 @@
+print('Olá, mundo!')
+
+print(7+4)
+print("7" + "4")
+print("7 + 4")
+# VARIAVEIS
+
+nome = "Davi"
+idade = 18
+peso = 70
+
+print(nome, idade, peso)
+print(f"Olá, {nome}!")
+
+# INPUTS --- SIMULAÇÃO DE UM FORMS NO CMO
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
+
---
+++ aula02desafio.py
@@ -0,0 +1,14 @@
+nome = input("Qual seu nome? ")
+print(f"Bem vindo {nome}")
+
+
+dia = input("Que dia você nasceu? ")
+mês = input("Que mês você nasceu? ")
+ano= input("Que ano você nasceu? ")
+
+print(f"Você nasceu no dia {dia}/{mês}/{ano}")
+
+num1 = float(input("Me dê um número :"))
+num2 = float(input("Me dê outro número:"))
+
+print(f"A soma entre esses dois números é {num1 + num2}")
