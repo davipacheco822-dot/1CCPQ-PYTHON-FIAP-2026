c8743b4234cca6800e71d7670c5ddb0b05fabb0a
labsfiap <davipacheco822@gmail.com>
2026-08-31 20:16:12 -0300

    cp05

---
+++ CP05-Russi/exercicios_cp05.py
@@ -0,0 +1,54 @@
+#registro = ("API-01" , [200, 200 , 500])
+
+#registro[1].append(404)
+
+#print(registro)
+
+#def registrar_acesso(dados):
+    #dados["acessos"] += 1
+    #return dados
+
+
+#sistema = {
+ #   "acessos": 10
+#}
+
+#resultado = registrar_acesso(sistema)
+
+#resultado["acessos"] +=5
+
+#print(sistema["acessos"])
+
+#logs = (
+  #  ("/login" , 200),
+  #  ("/pedidos" , 500),
+  #  ("/login" , 401),
+  #  ("/pedidos" , 201),
+  #  ("/login" , 200)
+#)
+
+#resultado = {}
+
+#for endpoint, codigo in logs:
+
+  #  if endpoint not in resultado:
+    #    resultado[endpoint] = [0,0]
+
+    #resultado[endpoint][0] += 1
+
+ #  if codigo >= 400:
+      #  resultado[endpoint][1] +=1
+
+#print(resultado)
+
+#status = {
+ #   "/login" : 200,
+  #  "/usuarios" : 404
+#}
+
+#print(
+   # "/login" in status,
+   #  200 in status,
+   # ("/login" , 200) in status.items()
+#)
+
